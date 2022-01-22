331ae08a05b9b2d91d8d3c9da375cba0dbdef773
Andrey <forestx24@gmail.com>
2022-01-22 12:50:03 +0100

    do header component

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title: string = 'cemeresi';
+  isScroling: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  @HostListener("document:scroll")
+  onScroll(): boolean {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      return this.isScroling = true;
+    }
+    return this.isScroling = false;
+  }
 }
