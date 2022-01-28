dfcef9b9c4db5f92b6172ba82690eb2c986721cf
Andrey <forestx24@gmail.com>
2022-01-28 19:35:57 +0100

    added something in services page

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  bounceInLeftOnEnterAnimation,
+  bounceInDownOnEnterAnimation
+} from 'angular-animations';
+
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
+  animations: [
+    bounceInDownOnEnterAnimation({ anchor: 'down' }),
+    bounceInDownOnEnterAnimation({ anchor: 'down2', delay: 100 }),
+  ]
 })
 export class ServicesComponent implements OnInit {
 
