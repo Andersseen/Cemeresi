a2bef1995a48112948967d387638a864ec5f0206
Andrey <forestx24@gmail.com>
2022-03-05 13:46:21 +0100

    added other child components in services

---
+++ src/app/pages/services/pages/cirurgia/cirurgia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cirurgia',
+  templateUrl: './cirurgia.component.html',
+  styleUrls: ['../../services.component.scss']
+})
+export class CirurgiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/services/services.module.ts
+++ src/app/pages/services/services.module.ts
@@ -10,6 +10,8 @@ import { FacialComponent } from './pages/integral/facial/facial.component';
 import { CorporalComponent } from './pages/integral/corporal/corporal.component';
 import { DieteticaComponent } from './pages/dietetica/dietetica.component';
 import { FisioterapiaComponent } from './pages/fisioterapia/fisioterapia.component';
+import { CirurgiaComponent } from './pages/cirurgia/cirurgia.component';
+import { PsicologiaComponent } from './pages/psicologia/psicologia.component';
 
 
 
@@ -19,7 +21,7 @@ import { FisioterapiaComponent } from './pages/fisioterapia/fisioterapia.compone
     ServicesRoutingModule,
     MaterialModule
   ],
-  declarations: [ServicesComponent, IntegralComponent, FacialComponent, CorporalComponent, DieteticaComponent, FisioterapiaComponent]
+  declarations: [ServicesComponent, IntegralComponent, FacialComponent, CorporalComponent, DieteticaComponent, FisioterapiaComponent, CirurgiaComponent, PsicologiaComponent]
 })
 export class ServicesModule { }
 
