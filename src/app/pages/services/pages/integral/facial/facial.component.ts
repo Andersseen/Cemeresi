3d53b5172d23c8bdf3ad4a202c8ced6f267ad9da
Andrey <forestx24@gmail.com>
2022-03-19 14:28:53 +0100

    changes view of services

--- src/app/pages/services/pages/integral/facial/facial.component.ts
+++ src/app/pages/services/pages/integral/facial/facial.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  slideInRightOnEnterAnimation
+} from 'angular-animations';
 
 @Component({
   selector: 'app-facial',
   templateUrl: './facial.component.html',
-  styleUrls: ['../../../services.component.scss', 'facial.component.scss']
+  styleUrls: ['../../../services.component.scss', 'facial.component.scss'],
+  animations: [
+    slideInRightOnEnterAnimation({ anchor: 'slide' }),
+  ]
 })
 export class FacialComponent implements OnInit {
+
+  typesOfShoes: string[] = ['LIMPIEZA FACIAL CON MICRODERMOABRASIÓN CON PUNTA DE DIAMANTE', 'TRATAMIENTOS PERSONALIZADOS', 'RADIOFRECUENCIA', 'OXIGENOTERAPIA H2O'];
+
+  isList = true
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doSlide() {
+    this.isList = !this.isList
   }
 
 }
